5516e8c57f38125b13af46a3b8ce824baaffb829
dragonite-lee <ross1222@naver.com>
2023-03-05 23:45:21 +0900

    정규식 :문자 추가

--- component/Login.tsx
+++ component/Login.tsx
@@ -21,7 +21,7 @@ function Login() {
         } else {
             setEmailValid(false);
         }
-        const regexPw = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+        const regexPw = /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$:!%*#^?&\\(\\)\-_=+]).{8,20}$/;
         if(regexPw.test(pw)) {
             setPwValid(true);
         } else {
